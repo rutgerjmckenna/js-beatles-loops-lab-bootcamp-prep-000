40137888e1e6f3c72c3cd6a5fef65d133014f921
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-04-15 17:46:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function johnLennonFacts() {
   const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var array = [];
   var i = 0;
-  while (array.length < facts.length) {
-    array.push(facts[i] + "!!!");
-    i++;
+  while (i < facts.length) {
+        array.push(facts[i] +"!!!");
+        i++;
   }
   return array
 }
