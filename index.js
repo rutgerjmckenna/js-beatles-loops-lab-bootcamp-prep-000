df1c518fc5b15df261400a6fc562ed141e545ab3
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-04-15 17:58:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
-function johnLennonFacts() {
-  const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+function johnLennonFacts(facts) {
   var array = [];
   var i = 0;
   while (i < facts.length) {
