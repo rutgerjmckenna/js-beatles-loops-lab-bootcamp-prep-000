f3927acd876cc8d438c5b7f75a3732e3e061104e
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-04-15 17:48:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function johnLennonFacts() {
   var array = [];
   var i = 0;
   while (i < facts.length) {
-        array.push(array[i] +"!!!");
+        array.push(facts[i] +"!!!");
         i++;
   }
   return array
